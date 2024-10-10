2fd0bbab34c93eabdc158f629b9e7bfd2f82c4e2
paweliwanicki <pawel_iwanicki@outlook.com>
2024-10-10 19:16:43 +0200

    add new  ghost enemy

--- scenes/game_object/ghost_enemy/ghost_enemy.gd
+++ scenes/game_object/ghost_enemy/ghost_enemy.gd
@@ -1,21 +1,22 @@
 extends CharacterBody2D
 
-@onready var visuals = $Visuals;	
+
+@onready var visuals = $Visuals;
 @onready var velocity_component = $VelocityComponent;
 
 
-func _ready():
+func _ready(): 
 	$HurtboxComponent.hit.connect(on_hit);
-
-
+	
+	
 func _process(delta):
 	velocity_component.accelerate_to_player();
 	velocity_component.move(self);
-
+	
 	var move_sign = sign(velocity.x);
 	if move_sign != 0:
-		visuals.scale = Vector2(move_sign, 1);
+		visuals.scale = Vector2(move_sign,1);
 	
-
+	
 func on_hit():
 	$HitRandomStreamPlayer2DComponent.play_random();
